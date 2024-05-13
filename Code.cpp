1d2cbc3c079a87634ac563b451c83fcf0a0a5647
Ruochen04 <145728288+Ruochen04@users.noreply.github.com>
2024-05-13 13:36:55 -0400

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -7,17 +7,24 @@
 
 using namespace std;
 
+// Constructor 
 Code::Code(int n, int m) {
-    this->n = n;
+    this->n = n; // Getting Private Members
     this->m = m;
 }
 
 void Code::initialize_randomly() {
+    
     secret_code.clear();
+
+    // Loop to create n length
     for (int i = 0; i < n; ++i) {
+
+        // Randomly Generate Number in M Range
         secret_code.push_back(rand() % m);
     }
 }
+
 
 int Code::check_correct(const vector<int>& guess) {
     int count = 0;
@@ -31,7 +38,7 @@ int Code::check_correct(const vector<int>& guess) {
 
 int Code::check_incorrect(const vector<int>& guess) {
     int count = 0;
-    std::vector<int> secret_copy = secret_code;
+    std::vector<int> secret_copy = secret_code; // Maybe add std:: if not working
     for (int i = 0; i < n; ++i) {
         auto it = std::find(secret_copy.begin(), secret_copy.end(), guess[i]);
         if (it != secret_copy.end()) {
