31323028980b8957f92083918bcca246714cb09c
Ruochen04 <145728288+Ruochen04@users.noreply.github.com>
2024-05-13 13:02:22 -0400

    Update code2.h
    comments

--- code2.h
+++ code2.h
@@ -7,20 +7,24 @@ using namespace std;
 
 class code {
 public:
+
+    // Constructor
     code(int m, int n);
 
-    vector<int> makeSequence(int n, int m);
-    vector<int> getSequence(int n);
-    int checkCorrect(std::vector<int> Sequence, std::vector<int> Guess, int n);
-    int checkIncorrect(std::vector<int> Sequence, std::vector<int> Guess, int n);
+    vector<int> makeSequence(int n, int m); // Generates Random Sequence Given Length (n) and Range (m)
+    vector<int> getSequence(int n); // Reads Input Sequence Guess from the user
+    int checkCorrect(vector<int> Sequence, vector<int> Guess, int n); // Check correct
+    int checkIncorrect(vector<int> Sequence, vector<int> Guess, int n); // Check incorrect
+    
+    void readInputs(); // Read Input from User for Length and Range
 
-    void readInputs();
+    // Get from Private
     int getLength() const;
     int getRange() const;
 
 private:
-    int length;
-    int range;
+    int length; // Code Length (n)
+    int range; // Code Range (m)
 };
 
 #endif // CODE_H
